713d4ce86731805b500f5e321a451057a6f42b95
Igor Svistun <igorekha2003@icloud.com>
2021-01-07 12:59:56 +0300

    'filter'

---
+++ src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem :product="product" v-for="product in products" :key="product.id"/>
+  </ul>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem';
+
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props: ['products'],
+};
+</script>
+
+<style scoped>
+
+</style>
