cd01e9f246b3e20b93a474836fed8831d270a5c4
Igor Svistun <igorekha2003@icloud.com>
2021-01-15 11:48:20 +0300

    'Cart'

--- src/helpers/numberFormat.js
+++ src/helpers/numberFormat.js
@@ -1,3 +1,7 @@
 export default function numberFormat(value){
+  if (typeof value === 'number')
+  {
+    console.log(typeof value)
     return new Intl.NumberFormat().format(value);
+  }
 }
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -26,44 +26,7 @@
       <form class="cart__form form" action="#" method="POST">
         <div class="cart__field">
           <ul class="cart__list">
-            <li class="cart__item product" v-for="item in products" :key="item.productId">
-              <div class="product__pic">
-                <img :src="item.product.img" width="120" height="120" :alt="item.product.title">
-              </div>
-              <h3 class="product__title">
-                {{ item.product.title }}
-              </h3>
-
-              <span class="product__code">
-                Артикул: {{ item.product.id }}
-              </span>
-
-              <div class="product__counter form__counter">
-                <button type="button" aria-label="Убрать один товар">
-                  <svg width="10" height="10" fill="currentColor">
-                    <use xlink:href="#icon-minus"></use>
-                  </svg>
-                </button>
-
-                <input type="text" :value="item.amount" name="count">
-
-                <button type="button" aria-label="Добавить один товар">
-                  <svg width="10" height="10" fill="currentColor">
-                    <use xlink:href="#icon-plus"></use>
-                  </svg>
-                </button>
-              </div>
-
-              <b class="product__price">
-                {{ item.amount * item.product.price | numberFormat}} ₽
-              </b>
-
-              <button class="product__del button-del" type="button" aria-label="Удалить товар из корзины">
-                <svg width="20" height="20" fill="currentColor">
-                  <use xlink:href="#icon-close"></use>
-                </svg>
-              </button>
-            </li>
+            <CartItem v-for="item in products" :key="item.productId" :item="item"/>
           </ul>
         </div>
 
@@ -74,7 +37,6 @@
           <p class="cart__price">
             Итого: <span>{{ totalPrice | numberFormat }} ₽</span>
           </p>
-
           <button class="cart__button button button--primery" type="submit">
             Оформить заказ
           </button>
@@ -87,15 +49,20 @@
 <script>
 
 import numberFormat from '@/helpers/numberFormat';
-import {mapGetters} from 'vuex'
+import { mapGetters } from 'vuex';
+import CartItem from '@/components/CartItem';
 
 export default {
   name: 'CartPage',
+  components: {CartItem},
   filters: {
     numberFormat
   },
   computed: {
-    ...mapGetters({products: 'cartDetailProducts', totalPrice: 'cartTotalPrice'}),
+    ...mapGetters({
+      products: 'cartDetailProducts',
+      totalPrice: 'cartTotalPrice'
+    }),
   }
 };
 </script>
